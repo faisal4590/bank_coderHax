f987db972582f67111f3c6e80b46b6f761b14c8e
faisal.aziz <faisal.aziz@NAZ-TECH-PC-103.ntdac.naztech.local>
2019-02-03 18:00:27 +0600

    new method

--- src/main/java/com/coderHax/DatabaseUtil/BankOperations.java
+++ src/main/java/com/coderHax/DatabaseUtil/BankOperations.java
@@ -4,5 +4,8 @@ import com.coderHax.Service.BankDAOImplementation;
 
 public class BankOperations {
   
-		
+		public static void main(String[] args) {
+			BankDAOImplementation bd = new BankDAOImplementation();
+			bd.coderHax1_customerName();
+		}
 }
